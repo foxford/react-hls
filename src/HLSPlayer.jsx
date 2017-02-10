99dab22a96389ca07de6c09d33c3e5bc91af7b7b
etolchennikov <e.tolchennikov@netology-group.ru>
2017-02-10 19:28:09 +0300

    try to load Hls video (1)

--- src/HLSPlayer.jsx
+++ src/HLSPlayer.jsx
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-window.Hls = require('hls.js'); // for Hls object visibility
+const Hls = require('hls.js/lib/hls').default;
 
 class HLSPlayer extends Component {
+
+  componentDidMount() {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+
+      hls.loadSource('http://www.streambox.fr/playlists/test_001/stream.m3u8');
+      hls.attachMedia(this.videoElement);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        this.videoElement.play();
+      });
+    }
+  }
+
   render() {
     return (
-      <div>
-        HLS Player!
-      </div>
+      <video ref={ (video) => { this.videoElement = video; } } />
     );
   }
 }
