ac084e2afdeab8be6da9977f3cb8fac47bd2754d
etolchennikov <e.tolchennikov@netology-group.ru>
2017-02-16 15:58:22 +0300

    add style changer to example

--- example/App.js
+++ example/App.js
@@ -5,7 +5,19 @@ import './style.css';
 
 class App extends Component {
 
+  state = {
+    styleType: 1
+  };
+
+  handleLogoClick(type) {
+    this.setState({
+      styleType: type
+    });
+  }
+
   render() {
+    const { styleType } = this.state;
+    const playerClass = `player player_${styleType}`;
     const source = 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8';
     const customControls = {
       panelBg: '#07141e',
@@ -23,8 +35,17 @@ class App extends Component {
 
     return (
       <div>
-        <img className="logo" src="http://foxford.ru/assets/logo-white-21110926a80c56997e1f164de0db94ed.svg" />
-        <div className="player">
+        <div className="logos">
+          <img className="logo"
+               src="https://netology-group.ru/assets/corpsite/logo/foxford-1f9bb97e5f7d3bab0d608c1f6d220d53.svg"
+               onClick={ this.handleLogoClick.bind(this,1) }
+          />
+          <img className="logo"
+               src="https://netology-group.ru/assets/corpsite/logo/netology-39abf60d361c76c80c577c311a5aa53f.svg"
+               onClick={ this.handleLogoClick.bind(this,2) }
+          />
+        </div>
+        <div className={playerClass}>
           <HLSPlayer source={source} customControls={customControls} />
         </div>
       </div>
