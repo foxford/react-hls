6fb308f3ec8045c0bbc28dfa785e63b359f972dd
alexkonst <alex_konst24@mail.ru>
2018-04-19 14:01:26 +0300

    MEDIA-1298 Changed hls.js import

--- dist/HLSSource.js
+++ dist/HLSSource.js
@@ -14,7 +14,7 @@ var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-require('!script-loader!hls.js');
+require('!script-loader!hls.js/dist/hls.min.js');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
